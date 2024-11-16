3d73e4c0029e35989cf902f7344cc3ca8d737d4b
enricodelguerra <ricodelguerra@gmail.com>
2024-11-16 14:33:39 -0300

    Método findById criado em AtivdadeDAO

--- src/main/java/br/com/fiap/dao/AtividadeDAO.java
+++ src/main/java/br/com/fiap/dao/AtividadeDAO.java
@@ -34,8 +34,29 @@ public class AtividadeDAO extends Repository{
             return atividades;
     }
 
-    public AtividadeTO findById() {
+    public AtividadeTO findById(Long id) {
         String sql = "select * from t_eco_atividade where id_atividade = ?";
-
+        try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+            ps.setLong(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                AtividadeTO atividade = new AtividadeTO();
+                atividade.setIdAtividade(rs.getLong("id_atividade"));
+                atividade.setId_projeto(rs.getLong("id_projeto"));
+                atividade.setNome(rs.getString("nome"));
+                atividade.setDescricao(rs.getString("descricao"));
+                atividade.setResponsavel(rs.getString("responsavel"));
+                atividade.setDataInicio(rs.getDate("data_inicio").toLocalDate());
+                atividade.setDataFim(rs.getDate("data_fim").toLocalDate());
+                return atividade;
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro de SQL" + e.getMessage());
+        } finally {
+            closeConnection();
+        }
+        return null;
     }
+
+
 }
