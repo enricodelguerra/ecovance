47fc3fc7d1ba3e13e3e431812c229eecf57d78c8
enricodelguerra <ricodelguerra@gmail.com>
2024-11-16 14:28:16 -0300

    Método findAll criado na classe AtividadeDAO

--- src/main/java/br/com/fiap/dao/AtividadeDAO.java
+++ src/main/java/br/com/fiap/dao/AtividadeDAO.java
@@ -1,4 +1,41 @@
 package br.com.fiap.dao;
 
-public class AtividadeDAO {
+import br.com.fiap.to.AtividadeTO;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class AtividadeDAO extends Repository{
+    public ArrayList<AtividadeTO> findAll() {
+        ArrayList<AtividadeTO> atividades = new ArrayList<>();
+        String sql = "Select * from t_eco_atividade";
+            try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+                ResultSet rs = ps.executeQuery();
+                if (rs != null) {
+                    while (rs.next()) {
+                        AtividadeTO atividade = new AtividadeTO();
+                        atividade.setIdAtividade(rs.getLong("id_atividade"));
+                        atividade.setId_projeto(rs.getLong("id_projeto"));
+                        atividade.setNome(rs.getString("nome"));
+                        atividade.setDescricao(rs.getString("descricao"));
+                        atividade.setResponsavel(rs.getString("responsavel"));
+                        atividade.setDataInicio(rs.getDate("data_inicio").toLocalDate());
+                        atividade.setDataFim(rs.getDate("data_fim").toLocalDate());
+                        atividades.add(atividade);
+                    }
+                }
+            } catch (SQLException e) {
+                System.out.println("Erro de SQL! " + e.getMessage());
+            } finally {
+                closeConnection();
+            }
+            return atividades;
+    }
+
+    public AtividadeTO findById() {
+        String sql = "select * from t_eco_atividade where id_atividade = ?";
+
+    }
 }
