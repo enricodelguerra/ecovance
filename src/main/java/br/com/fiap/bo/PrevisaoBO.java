0b90b71ac03b4a901f0a535eaf93bf364514b3bd
enricodelguerra <ricodelguerra@gmail.com>
2024-11-18 08:44:12 -0300

    Classe PrevisaoBO finalizada

--- src/main/java/br/com/fiap/bo/PrevisaoBO.java
+++ src/main/java/br/com/fiap/bo/PrevisaoBO.java
@@ -1,4 +1,34 @@
 package br.com.fiap.bo;
 
+import br.com.fiap.dao.PrevisaoDAO;
+import br.com.fiap.to.PrevisaoTO;
+
+import java.util.ArrayList;
+
 public class PrevisaoBO {
+    PrevisaoDAO PrevisaoDAO;
+
+    public ArrayList<PrevisaoTO> finsAll() {
+        PrevisaoDAO = new PrevisaoDAO();
+        return PrevisaoDAO.findAll();
+    }
+
+    public PrevisaoTO findById(Long id) {
+        PrevisaoDAO = new PrevisaoDAO();
+        return PrevisaoDAO.findByid(id);
+    }
+
+    public PrevisaoTO save(PrevisaoTO previsao) {
+        PrevisaoDAO = new PrevisaoDAO();
+        return PrevisaoDAO.save(previsao);
+    }
+
+    public PrevisaoTO edit(Long id, PrevisaoTO previsao) {
+        PrevisaoDAO = new PrevisaoDAO();
+        return PrevisaoDAO.edit(id, previsao);
+    }
+
+    public boolean delete(Long codigo) {
+        return PrevisaoDAO.delete(codigo);
+    }
 }
