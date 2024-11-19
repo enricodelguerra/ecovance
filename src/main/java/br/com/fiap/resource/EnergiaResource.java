402a552b6103560126771c3b53e11fa60c6cb6ce
enricodelguerra <ricodelguerra@gmail.com>
2024-11-19 17:20:37 -0300

    Classe EnergiaResource adicionada ao projeto

---
+++ src/main/java/br/com/fiap/resource/EnergiaResource.java
@@ -0,0 +1,82 @@
+package br.com.fiap.resource;
+
+import br.com.fiap.bo.EnergiaBO;
+import br.com.fiap.to.EnergiaTO;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.ArrayList;
+
+@Path("/ecovance/energia")
+public class EnergiaResource {
+    private EnergiaBO energiaBO = new EnergiaBO();
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findAll() {
+        ArrayList<EnergiaTO> resultado = energiaBO.findAll();
+        Response.ResponseBuilder response = null;
+        if (resultado != null) {
+            response = Response.ok(); //200 (OK)
+        } else {
+            response = Response.status(404); // 404 (NOT FOUND)
+        }
+        response.entity(resultado);
+        return response.build();
+    }
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findById(@PathParam("id") Long id) {
+        EnergiaTO resultado = energiaBO.findById(id);
+        Response.ResponseBuilder response = null;
+        if (resultado != null) {
+            response = Response.ok(); // 200 (OK)
+        } else {
+            response = Response.status(404); // 404 (NOT FOUND)
+        }
+        response.entity(resultado);
+        return response.build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response save(EnergiaTO energia) {
+        EnergiaTO resultado = energiaBO.save(energia);
+        Response.ResponseBuilder response = null;
+        if (resultado != null) {
+            response = Response.created(null); // 201 - CREATED
+        } else {
+            response = Response.status(400); // 401 - BAD REQUEST
+        }
+        response.entity(resultado);
+        return response.build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Response delete(Long id) {
+        Response.ResponseBuilder response = null;
+        if (energiaBO.delete(id)) {
+            response = Response.status(204); // 204 - NO CONTENT
+        } else {
+            response = Response.status(404); // 404 - NOT FOUND
+        }
+        return response.build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response edit(EnergiaTO energia, @PathParam("id") Long id) {
+        energia.setIdFonteEnergia(id);
+        EnergiaTO resultado = energiaBO.edit(energia);
+        Response.ResponseBuilder response = null;
+        if (resultado != null) {
+            response = Response.created(null); // 201 - CREATED
+        } else {
+            response = Response.status(400); // - BAD REQUEST
+        }
+        return response.build();
+    }
+}
